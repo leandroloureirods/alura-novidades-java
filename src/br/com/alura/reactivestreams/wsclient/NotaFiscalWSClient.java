db2770a033d5dad13a94e46dc21fc963727933a6
Leandro Loureiro dos Santos <leandroloureirods@gmail.com>
2022-12-02 12:10:08 -0500

    Testando Reactive Streams

---
+++ src/br/com/alura/reactivestreams/wsclient/NotaFiscalWSClient.java
@@ -0,0 +1,16 @@
+package br.com.alura.reactivestreams.wsclient;
+
+import br.com.alura.reactivestreams.model.NotaFiscal;
+
+public class NotaFiscalWSClient {
+
+	public void enviar(NotaFiscal nf) {
+		try {
+			System.out.println("Emitindo nota fiscal");
+			Thread.sleep(5000);
+			System.out.format("Nota fiscal emitida %nEm nome de %s %nNa data de %s %n", nf.getNome(), nf.getData());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
